4cd57e99b9cc71847ae7428d9697480bb91decad
npador07 <npador07@gmail.com>
2026-02-12 13:09:38 +0800

    updated sw

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,13 @@
-const CACHE_NAME = "weather-pwa-v1";
-const ASSETS = [
-  "/",
-  "/offline.html"
-];
+const CACHE_NAME = "weather-pwa-v2";
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./offline.html"
+      ]);
+    })
   );
   self.skipWaiting();
 });
@@ -18,11 +19,7 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   if (event.request.mode === "navigate") {
     event.respondWith(
-      fetch(event.request).catch(() => caches.match("/offline.html"))
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then(res => res || fetch(event.request))
+      fetch(event.request).catch(() => caches.match("./offline.html"))
     );
   }
 });
